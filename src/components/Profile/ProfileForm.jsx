e2d801c9f393e7327ff62807a76400053e9d3cfc
Hwirin-Kim <kimhuirin@gmail.com>
2022-11-01 19:13:48 +0900

    프로필 수정기능 수정중(탑,미들 합치기 직전)

--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 import defaultPic from "../../img/kakaodefault.jpg";
 import { BsCameraFill } from "react-icons/bs";
-function ProfileForm({ profileEdit, setProfileEdit }) {
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { __getFriendProfileById } from "../../Redux/module/friend";
+import { createSearchParams } from "react-router-dom";
+
+function ProfileForm({ profileEdit, setProfileEdit, profileData, userInfo }) {
+  const dispatch = useDispatch();
+  console.log("유저인포", userInfo);
+  let inputRef;
+  useEffect(() => {
+    dispatch(__getFriendProfileById(profileData));
+  }, [dispatch, profileData]);
+
+  const friendData = useSelector(
+    ({ friendReducer }) => friendReducer.friendProfile
+  );
+  const [selectData, setSelectData] = useState();
+  const [editImage, setEditImage] = useState();
+  useEffect(() => {
+    const data = () => {
+      if (userInfo) {
+        setSelectData(userInfo);
+      } else {
+        setSelectData(friendData);
+      }
+    };
+    data();
+  }, [userInfo, friendData]);
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setSelectData({ ...selectData, [name]: value });
+    console.log("온체인지", selectData);
+  };
+  console.log("셀렉트데이터", selectData);
+
+  const onChangeImage = (e) => {
+    e.preventDefault();
+    const previewUrl = URL.createObjectURL(e.target.files[0]);
+    setEditImage(e.target.files[0]);
+  };
+
   return (
     <StMid>
       {profileEdit ? null : (
@@ -9,15 +50,49 @@ function ProfileForm({ profileEdit, setProfileEdit }) {
           <BsCameraFill size={20} />
         </div>
       )}
-      <div className="pic">
-        <img src={defaultPic} />
-      </div>
+
+      {profileEdit ? (
+        <div className="pic">
+          <img src={selectData?.image} alt="profile" />
+        </div>
+      ) : (
+        <>
+          <div className="pic" onClick={() => inputRef.click()}>
+            <img src={selectData?.image} alt="profile" />
+            <input
+              className="img-input"
+              type="file"
+              accept="image/*"
+              name="image_file"
+              onChange={onChangeImage}
+              //인풋을 안보이게하고 버튼에 레퍼런스 입력함
+              ref={(refParam) => (inputRef = refParam)}
+            />
+          </div>
+        </>
+      )}
 
       <div className="nick">
-        {profileEdit ? <span>닉네임</span> : <input />}
+        {profileEdit ? (
+          <span>{selectData?.nickname}</span>
+        ) : (
+          <input
+            name="nickname"
+            value={selectData.nickname}
+            onChange={onChangeHandler}
+          />
+        )}
       </div>
       <div className="sangme">
-        {profileEdit ? <span>상태메시지</span> : <input />}
+        {profileEdit ? (
+          <span>{selectData?.status}</span>
+        ) : (
+          <input
+            name="status"
+            value={selectData.status}
+            onChange={onChangeHandler}
+          />
+        )}
       </div>
     </StMid>
   );
@@ -51,6 +126,9 @@ const StMid = styled.div`
     overflow: hidden;
     position: relative;
     z-index: 1;
+    .img-input {
+      display: none;
+    }
 
     img {
       /* position: absolute; */
